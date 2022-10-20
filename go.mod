09f1f5f632ed7aba4b5a6d8546598010069244ba
NyanCatda <nyancatda@outlook.com>
2022-10-20 17:07:19 +0800

    初始化项目

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nyancatda/Cherino
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+/*
+ * @Author: NyanCatda
+ * @Date: 2022-10-20 17:06:29
+ * @LastEditTime: 2022-10-20 17:06:32
+ * @LastEditors: NyanCatda
+ * @Description: 主文件
+ * @FilePath: \Cherino\main.go
+ */
+package main
